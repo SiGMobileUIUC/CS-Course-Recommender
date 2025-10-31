2cfdbf115b567369b28ffa3dcf3685c35fd12b7a
rmandell <robert@apsdatatechnologies.com>
2025-10-31 14:50:39 -0500

    Closes 10

---
+++ course-info-getter/GPAParser.py
@@ -0,0 +1,114 @@
+import csv
+import json
+import requests
+from io import StringIO
+
+
+def search_file():
+    cs_classes = []
+    COURSE_TYPE_INDEX = 3
+
+    url = 'https://raw.githubusercontent.com/wadefagen/datasets/refs/heads/main/gpa/uiuc-gpa-dataset.csv'
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    csv_data = StringIO(response.text)
+    csvFile = csv.reader(csv_data)
+
+    for lines in csvFile:
+        #print(lines)
+        if lines[COURSE_TYPE_INDEX] == "CS":
+            cs_classes.append(lines)
+
+    return cs_classes
+
+
+def add_arrays_by_index(arr1, arr2):
+    if len(arr1) != len(arr2):
+        raise ValueError("Arrays must be of equal length to add element-wise.")
+
+    result_array = []
+    for i in range(len(arr1)):
+        result_array.append(arr1[i] + arr2[i])
+    return result_array
+
+
+def get_class_grades_dict(data):
+    NAME_INDEX = 5
+
+    GRADES_INDEX_START = 7
+    GRADES_INDEX_END = 20
+
+    name = data[0][NAME_INDEX]
+    grades = [int(item) for item in data[0][GRADES_INDEX_START:GRADES_INDEX_END]]
+    class_dict = {name: grades}
+    for i in range(1, len(data)):
+        line_data = [int(item) for item in data[i][GRADES_INDEX_START:GRADES_INDEX_END]]
+        line_name = data[i][NAME_INDEX]
+        if line_name in class_dict:
+            temp_grades = add_arrays_by_index(class_dict[line_name], line_data)
+            class_dict[line_name] = (temp_grades)
+        else:
+            temp_dict = {line_name: line_data}
+            class_dict.update(temp_dict)
+
+    return class_dict
+
+def find_class_gpa(data):
+    gpa_scale = [4.0, 4.0, 3.67, 3.33, 3.0, 2.67, 2.33, 2.0, 1.67, 1.33, 1.0, 0.67, 0.0]
+
+    class_gpa_dict = {}
+
+    for key in data:
+        grades = data[key]
+        students = 0
+        sum = 0.0
+        for i in range(0, len(grades)):
+            students += grades[i]
+            sum += grades[i] * gpa_scale[i]
+        class_gpa_dict.update({key: round(sum / students, 2)})
+
+    return class_gpa_dict
+
+def get_names(data):
+    class_names = []
+
+    CLASS_NAME_INDEX = 4
+
+    for array in data:
+        if not class_names.__contains__(array[CLASS_NAME_INDEX]):
+            class_names.append(array[CLASS_NAME_INDEX])
+
+    return class_names
+
+def final_info(class_dict, names):
+    info = []
+    index = 0
+    for key in class_dict:
+        info_dict = {"ID" : names[index], "NAME": key, "GPA" : class_dict[key]}
+        info.append(info_dict)
+
+    return info
+
+
+# Press the green button in the gutter to run the script.
+def main():
+    data = search_file()
+    class_dict = get_class_grades_dict(data)
+    vals = find_class_gpa(class_dict)
+    names = get_names(data)
+    final = final_info(vals, names)
+
+    with open('class_gpa.json', 'w') as json_file:
+        json.dump(final, json_file, indent=4)
+
+    print(f"Successfully processed {len(final)} CS courses")
+    print(f"Output saved to class_gpa.json")
+
+
+if __name__ == '__main__':
+    main()
+
+
+
